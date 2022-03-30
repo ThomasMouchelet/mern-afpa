d482df5d94144addf577de0b0cee9c0e30628956
ThomasMouchelet <mouchelet.thomas@gmail.com>
2022-03-30 13:48:46 +0200

    init

--- api/index.js
+++ api/index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const CustomerController = require('./src/controllers/customer.controller')
 
-app.post('/api/cutomers', function (req, res) {
+app.use(express.json())
+
+app.get('/', function (req, res) {
+  res.send('Hello')
+})
+
+app.post('/api/customers', function (req, res) {
   CustomerController.create(req, res)
 })
 
--- api/src/controllers/customer.controller.js
+++ api/src/controllers/customer.controller.js
@@ -2,9 +2,10 @@ const Customer = require('../models/customer.model');
 
 const CustomerController = {
     create: async (req, res) => {
+        console.log(req.body)
         const customer = new Customer(req.body);
-        await customer.save();
-        res.send('success')
+        // await customer.save();
+        res.send(customer)
     }
 }
 

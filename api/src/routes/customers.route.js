bf06980876669c8fa54061de876fea5a3dc2c569
ThomasMouchelet <mouchelet.thomas@gmail.com>
2022-04-15 10:07:27 +0200

    middleware

--- api/src/routes/customers.route.js
+++ api/src/routes/customers.route.js
@@ -2,8 +2,14 @@ const express = require('express')
 const CustomerController = require('../controllers/customer.controller')
 const router = express.Router()
 
+function middlewarTest(req, res, next) {
+    console.log("middlewarTest")
+
+    next()
+}
+
 router.post('/customers', CustomerController.create)
-router.get('/customers', CustomerController.findAll)
+router.get('/customers', middlewarTest, CustomerController.findAll)
 router.get('/customers/:id', CustomerController.findOne)
 router.get('/customers/:id/invoices', CustomerController.findAllInvoices)
 router.delete('/customers/:id', CustomerController.delete)
